0c3b35b5fcab090fe2a3eae10d6d4cd37800f1dc
Facundo-Beumont <fbeumont@mail.austral.edu.ar>
2024-06-14 09:18:06 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -19,7 +19,7 @@ class Authentication {
         passport.use(new GoogleStrategy({
             clientID: GOOGLE_CLIENT_ID,
             clientSecret: GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/auth/google/callback",
+            callbackURL: "https://tg-api-cl2f.onrender.com",
             passReqToCallback: true
         }, this.verifyIdentity));
 
